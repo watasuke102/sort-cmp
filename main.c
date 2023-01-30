4b7c4a32c9e8f70eb6178b1dca5538ceaddfc2fb
watasuke102 <watasuke102@gmail.com>
2023-01-30 10:30:49 +0900

    update: initialize array

--- main.c
+++ main.c
@@ -10,6 +10,16 @@ int main(void) {
     fprintf(stderr, "Failed to allocate array\n");
     return 1;
   }
+
+  for (int i = 0; i < len; ++i) {
+    printf("data[%2d]: ", i);
+    scanf("%d", data + i);
+  }
+  printf("=====\n");
+  for (int i = 0; i < len; ++i) {
+    printf("data[%2d]: %d\n", i, data[i]);
+  }
+
   free(data);
   return 0;
 }
